ee80c6a02484909cb36f1f96fd3c550b5d2c61bf
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2016-06-07 00:24:34 +0800

    adding test cases for parsing

--- test/acie_parser_test.erl
+++ test/acie_parser_test.erl
@@ -45,3 +45,33 @@ add_term_3_init_case_6_test() ->
     WT = acie_parser:add_term([], Term, undefined),
     EWT = {error, bad_term},
     ?assertEqual(EWT, WT).
+
+add_term_3_case_1_test() ->
+
+    Term = #term{ type= ?numeral, loc = 7, value= "12" },
+    WT = [#expression{ left= Term }],
+    WT1 = acie_parser:add_term(WT, Term, Term),
+    EWT = {error, bad_term},
+    ?assertEqual(EWT, WT1).
+
+add_term_3_case_2_test() ->
+
+    Term = #term{ type= ?numeral, loc = 8, value= "34" },
+    Term1 = #term{ type= ?op_mul, loc= 1, value= "*" },
+    WT = [#expression{ op= Term1, left= Term }],
+    WT1 = acie_parser:add_term(WT, Term, Term1),
+    EWT = [#expression{ full= true, op= Term1, left= Term, right= Term }],
+    ?assertEqual(EWT, WT1).
+
+add_term_3_case_3_test() ->
+
+    Term = #term{ type= ?rparan, loc= 9, value= ")" },
+    Term1 = #term{ type= ?numeral, loc= 3, value= "56" },
+    WT = [Exp =
+	      #expression{ full= true,
+			   op= #term{ type= ?op_add, loc= 2, value= "+" },
+			   left= #term{ type= ?float, loc= 1, value= "3,14" },
+			   right= Term1 }],
+    WT1 = acie_parser:add_term(WT, Term, Term1),
+    EWT = [#expression{ left= Exp }],
+    ?assertEqual(EWT, WT1).
